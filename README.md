d700878d6c7fdad280648291d04e1c5893a9e5e7
tangsj <t_fate@163.com>
2016-09-07 22:17:29 +0800

    添加 Todolist

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 基于 antd react 构建一个后台管理系统模板
 
+首先确保电脑上已安装nodejs 和 php 以及php包管理  Composer
+
+clone 项目后需要在根目录下执行
+
+npm install
+
+在services 目录下执行
+
+composer install
+
+为Laravel生成密钥
+
+php artisan key:generate
+
+
 ###启动方式
 
 ```
@@ -9,3 +24,8 @@
   npm run build:local -- 本地生产环境打包
   npm run build:server -- 正式生产环境打包
 ```
+
+
+### TODO
+
+参看dashboard.jsx
--- src/js/components/layout/dashboard.jsx
+++ src/js/components/layout/dashboard.jsx
@@ -1,10 +1,13 @@
-import { Card, Breadcrumb, Icon } from 'antd';
+import { Card, Breadcrumb, Icon, Badge } from 'antd';
 
 class Dashboard extends React.Component {
     constructor(props) {
       super(props);
       this.displayName = 'Dashboard';
       this._isMounted = false;
+      this.state = {
+        loading: true
+      }
     }
     componentWillUnmount() {
       this._isMounted = false;
@@ -19,21 +22,30 @@ class Dashboard extends React.Component {
             <Breadcrumb.Item><Icon type="home"/>首页</Breadcrumb.Item>
           </Breadcrumb>
 
-          <Card className="card-item" title="系统公告" extra={<a href="#">更多</a>} style={{ width: 300 }}>
-            <p>系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告系统公告</p>
+          <Card className="card-item" title="代办事项" extra={<Badge count={11}/>} style={{ width: 300 }}>
+            <p>1. AE/服务机构 添加状态管理，实现软删除</p>
+
+            <p>2. 添加项目流水时，AE选项根据所属项目进行动态加载</p>
+
+            <p>3. 流水开始和结束时间显示时分秒</p>
+
+            <p>4. 项目列表添加项目状态管理</p>
+
+            <p>5. 登录用户添加修改密码</p>
+
+            <p>6. 对登录后token有效时间进行限制</p>
+
+            <p>7. 将项目所有流水金额汇总到项目列表中</p>
+
+            <p>8. 添加系统用户及权限管理</p>
+
+            <p>9. 项目流水时间过滤</p>
+
+            <p>10. 项目流水导出Excel</p>
+
+            <p>11. 流水添加修改功能</p>
           </Card>
 
-          <Card className="card-item" title="代办事项" extra={<a href="#">更多</a>} style={{ width: 300 }}>
-            <p>1. 代办事项1</p>
-            <p>2. 代办事项2</p>
-            <p>3. 代办事项3</p>
-          </Card>
-
-          <Card className="card-item" title="今日订单" extra={<a href="#">更多</a>} style={{ width: 300 }}>
-            <p>今日订单</p>
-            <p>今日订单</p>
-            <p>今日订单</p>
-          </Card>
         </div>
       );
     }
